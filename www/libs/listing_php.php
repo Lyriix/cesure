99b76bff0659d1ce031f3bd8a020f5930cbf5508
Lyriix <lyriix@live.fr>
2016-08-02 23:55:07 -0400

    ajout des options de tri du listing

--- www/libs/listing_php.php
+++ www/libs/listing_php.php
@@ -1,5 +1,6 @@
 <?php 
-
+ini_set('display_errors','on');
+error_reporting(E_ALL);
 /* Réalisation de la procédure d'inscriptionlibs : Recuperation et affichage des listings */
 
 //Conexion à la BDD
@@ -14,12 +15,51 @@ catch(Exception $e)
 }
 
 
-//On doit recuperer les cesures et les personnes associés : jointure ?
+//Faire le tri par critère/colonne
+//include('fonc/db.php');
 
-$req_cesure = $bdd->query('SELECT num_cesure, filiere, majeure, ville, descriptif, duree_mois, entreprise, pays, continent FROM tab_cesure');
+$list_col = array( 'filiere','majeure','entreprise','duree_mois','pays','ville');
+$list_ord = array( 'asc','desc');
+$col = ( isset($_GET['col']) AND in_array(strtolower($_GET['col']),$list_col)) ? $_GET['col'] : 'pays' ;
+$ord = ( isset($_GET['ord']) AND in_array(strtolower($_GET['ord']),$list_ord)) ? strtoupper($_GET['ord']) : 'ASC' ;
 
-echo '<table>';
-echo '<tr><th>Filiere</th><th>Majeure</th><th>Entreprise</th><th>Duree</th><th>Pays</th><th>Ville</th><th>Descriptif</th><th></th></tr>';
+?>
+
+<table>
+    <tr>
+        <th>Filiere
+            <a href="?col=filiere&ord=asc"><img src="/www/images/icon/arrow_tri_up.png"></a>
+            <a href="?col=filiere&ord=desc"><img src="/www/images/icon/arrow_tri_down.png"></a>
+        </th>
+        <th>Majeure
+            <a href="?col=majeure&ord=asc"><img src="/www/images/icon/arrow_tri_up.png"></a>
+            <a href="?col=majeure&ord=desc"><img src="/www/images/icon/arrow_tri_down.png"></a>
+        </th>
+        <th>Entreprise
+            <a href="?col=entreprise&ord=asc"><img src="/www/images/icon/arrow_tri_up.png"></a>
+            <a href="?col=entreprise&ord=desc"><img src="/www/images/icon/arrow_tri_down.png"></a>
+        </th>
+        <th>Duree
+            <a href="?col=duree_mois&ord=asc"><img src="/www/images/icon/arrow_tri_up.png"></a>
+            <a href="?col=duree_mois&ord=desc"><img src="/www/images/icon/arrow_tri_down.png"></a>
+        </th>
+        <th>Pays
+            <a href="?col=pays&ord=asc"><img src="/www/images/icon/arrow_tri_up.png"></a>
+            <a href="?col=pays&ord=desc"><img src="/www/images/icon/arrow_tri_down.png"></a>
+        </th>
+        <th>Ville
+            <a href="?col=ville&ord=asc"><img src="/www/images/icon/arrow_tri_up.png"></a>
+            <a href="?col=ville&ord=desc"><img src="/www/images/icon/arrow_tri_down.png"></a>
+        </th>
+        <th>Descriptif</th><th></th>
+    </tr>
+    
+<?php
+
+    $req_cesure = $bdd->query("SELECT num_cesure, filiere, majeure, ville, descriptif, duree_mois, entreprise, pays, continent FROM tab_cesure ORDER BY $col $ord");
+    
+   
+
 while($donnees = $req_cesure->fetch())
 {
 ?>
